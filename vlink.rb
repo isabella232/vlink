da6d7fcbe8a052fb2d74a2f6018b3c1cbf852cc8
Tim Boring <tjboring@gmail.com>
2016-08-17 22:04:21 -0400

    Changed storage from Redis to Postgres

--- vlink.rb
+++ vlink.rb
@@ -1,78 +1,10 @@
+require 'data_mapper'
 require 'json'
-require 'pry'
-require 'redis'
-require 'sinatra'
-require 'sinatra/form_helpers'
-
-VLINK_HASH_KEY = 'vlink'
+require 'pg'
 
 class Link
-  def initialize(link, target)
-    @link = link
-    @target = target
-    @owners = []
-  end
+  include DataMapper::Resource
 
-  def to_json(*a)
-    {"#{@link}": {"target": "#{@target}", "owners": "#{@owners}"}}.to_json(*a)
-  end
-
-  def to_s
-    {"#{@link}": {"target": "#{@target}", "owners": "#{@owners}"}}.to_s
-  end
-
-  def validate()
-  end
-end
-
-helpers do
-  def get_link(link)
-    return settings.redis.hget(VLINK_HASH_KEY, link)
-  end
-
-  def get_links()
-    return settings.redis.hgetall(VLINK_HASH_KEY)
-  end
-
-  def create_link(link, target)
-    settings.redis.hset(VLINK_HASH_KEY, link, target)
-  end
-end
-
-configure do
-  set :redis, Redis.new(:url => (ENV['REDIS_PROVIDER'] || 'redis://127.0.0.1:6379/'))
-end
-
-get ('/') do
-  @error = params['error'] if !params['error'].nil?
-  @links = get_links()
-  erb :index
-end
-
-get ('/:link') do
-  link = params['link']
-  site = get_link(link)
-  redirect to(site) if !site.nil?
-  status 404
-end
-
-get ('/error') do
-  erb :error
-end
-
-# TODO(tjb): this method needs to save keys in a separate namespace per user.
-# User A should be able to have a link named 'foo' that points to
-# 'www.google.com', while user B should be able to have a link named 'bar'
-# that also points to 'www.google.com'. Links themselves, however, must
-# be unique, i.e. there can only be one http://vlink/foo
-post ('/') do
-  request.body.rewind
-  data = request.POST['link']
-  site = get_link(data['link'])
-  if !site.nil?
-    @error = ["#{data['link']} is already linked to #{site}"]
-    redirect to ("/?error=@error")
-  end
-  create_link(data['link'], data['target'])
-  redirect to('/') if status 201
+  property :link, String, :key => true
+  property :target, URI, :required => true
 end
