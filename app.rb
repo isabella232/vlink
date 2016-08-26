22543026e7a2dd042a10cb2f6cc03c08899a5db9
Tim Boring <tjboring@gmail.com>
2016-08-26 09:15:51 -0400

    UI and form cleanup

--- app.rb
+++ app.rb
@@ -20,6 +20,9 @@ configure do
   set :port, 8080
   set :environment, :production
   set :static_cache_control, [:public, max_age: 60 * 60 * 24 * 7]
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
 end
 
 get ('/') do
@@ -33,21 +36,25 @@ end
 
 get ('/:link') do
   link = Link.first(:name => params['link'])
-  session[:error_messages] = "/#{params['link']} does not exist. Create it?"
-  session[:link_name] = "#{params['link']}"
-  redirect :'/' if link.nil?
+  if link.nil?
+    session[:error_messages] = "/#{params['link']} does not exist. Create it?"
+    session[:link_name] = "#{params['link']}"
+    redirect :'/'
+  end
   link.used += 1
   link.save!
   redirect to(link.target.to_s)
 end
 
-get ('/search/:query') do
-  @links = Link.all(:name.like => "%#{params['query']}%")
+post ('/search') do
+  query = params['query']
+  puts "[DEBUG] Search for query: #{query}"
+  @links = Link.all(:name.like => "%#{query}%")
   erb :search
 end
 
 post ('/') do
-  data = request.POST['link']
+  data = {:name => request.POST['name'], :target => request.POST['target']}
   @link = Link.new(data)
   @link.save
   if !@link.saved?
